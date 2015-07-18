4a90d4a6bbe27839f0d379e092e455c99ad4734f
Ning Sun <sunng@about.me>
2015-07-18 21:04:10 +0800

    (feat) added :prepend and append support
    this allows user to specify dependencies for -Xbootclasspath/p: and
    -Xbootclasspath/a: separately.

--- src/lein_bootclasspath_deps/plugin.clj
+++ src/lein_bootclasspath_deps/plugin.clj
@@ -3,19 +3,36 @@
             [leiningen.core.classpath :as cp]
             [cemerick.pomegranate.aether :as aether]))
 
+(defn- path-for-deps [deps project]
+  (when (not-empty deps)
+    (let [btcp-deps-tree (cp/get-dependencies :boot-dependencies project)
+          hierarchy (aether/dependency-hierarchy deps btcp-deps-tree)
+          deps-paths (aether/dependency-files hierarchy)]
+      (clojure.string/join java.io.File/pathSeparatorChar
+                           (map str deps-paths)))))
+
+(defn- deps-type [dep]
+  (let [[_ _ & {:keys [prepend]}] dep]
+    (if prepend :prepend :append)))
+
 (defn middleware [project]
   (if-let [deps (->> project
                      :boot-dependencies
                      not-empty)]
     (do
       (debug "bootclasspath dependencies:" deps)
-      (let [btcp-deps-tree (cp/get-dependencies :boot-dependencies project)
-            hierarchy (aether/dependency-hierarchy deps btcp-deps-tree)
-            deps-paths (aether/dependency-files hierarchy)
+      (let [{prepends :prepend appends :append}
+            (group-by deps-type deps)
 
-            concat-path (clojure.string/join java.io.File/pathSeparatorChar
-                                             (map str deps-paths))]
-        (debug "bootclasspath dependencies:" concat-path)
-        (update-in project [:jvm-opts] conj
-                   (str "-Xbootclasspath/p:"  concat-path))))
+            [prepend-paths append-paths]
+            (map #(path-for-deps % project) [prepends appends])]
+        (debug "prepend bootclasspath:" prepend-paths)
+        (debug "append bootclasspath:" append-paths)
+        (-> project
+            (update-in [:jvm-opts] concat
+                       (when prepend-paths
+                         [(str "-Xbootclasspath/p:" prepend-paths)]))
+            (update-in [:jvm-opts] concat
+                       (when append-paths
+                         [(str "-Xbootclasspath/a:" append-paths)])))))
     project))
