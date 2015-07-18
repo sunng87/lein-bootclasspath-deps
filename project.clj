c80d8af9513119140185a9e6c83152beb5b9d5b7
Ning Sun <sunng@about.me>
2015-07-18 21:13:33 +0800

    Version 0.2.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject info.sunng/lein-bootclasspath-deps "0.2.0-SNAPSHOT"
+(defproject info.sunng/lein-bootclasspath-deps "0.2.0"
   :description "A Leiningen plugin to manage your bootclasspath."
   :url "https://github.com/sunng87/lein-bootclasspath-deps"
   :license {:name "Eclipse Public License"
